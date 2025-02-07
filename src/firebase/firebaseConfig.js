806dabca5c5dde5340cbbeb75b41a9362f7c6944
Hanson <Hansonlau7@gmail.com>
2025-02-06 23:48:06 -0800

    cicd: secrets can't be accessed by vercel unless prefix by NEXT_PUBLIC_ (adding them back)

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "profesite-43e7d.firebaseapp.com",
     projectId: "profesite-43e7d",
     storageBucket: "profesite-43e7d.firebasestorage.app",
     messagingSenderId: "317207705790",
-    appId: process.env.FIREBASE_APP_ID
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
   };
   
   export default firebaseConfig
