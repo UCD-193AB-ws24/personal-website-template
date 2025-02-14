c8cdff099c598db9b4f6a8b5e89a83d302306739
Lauren Mandell <108501401+laurenmandell@users.noreply.github.com>
2025-02-14 14:07:06 -0800

    style: Remove instances of dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import logo from "@public/logo.png";
+import Image from "next/image";
 
 interface NavLink {
   label: string;
@@ -17,7 +19,13 @@ const Navbar: React.FC<NavbarProps> = ({ user, onSignOut, logoHref = "/", navLin
     <nav className="border-[#00f2ff] bg-gray-900 overflow-hidden top-0 left-0 w-full z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href={logoHref} className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="logo.png" className="h-8" alt="Profesite Logo" />
+          <Image
+								src={logo}
+                width={32}
+                height={32}
+								className="h-8"
+								alt="Profesite Logo"
+					/>
           <span className="self-center text-4xl font-light tracking-wide whitespace-nowrap text-white iceland-font">
             PROFESITE
           </span>
@@ -25,7 +33,7 @@ const Navbar: React.FC<NavbarProps> = ({ user, onSignOut, logoHref = "/", navLin
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -48,14 +56,27 @@ const Navbar: React.FC<NavbarProps> = ({ user, onSignOut, logoHref = "/", navLin
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           {user ? (
-            <button
-              onClick={onSignOut}
-              className="relative px-6 py-3 font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 cursor-pointer"
-            >
-              Log Out
-            </button>
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
+                {navLinks.map((link) => (
+                  <li key={link.href}>
+                    <Link
+                      href={link.href}
+                      className="relative px-6 py-3 font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50"
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+
+                <button
+                  onClick={onSignOut}
+                  className="relative px-6 py-3 font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 cursor-pointer"
+                >
+                  Log Out
+                </button>
+              </ul>
           ) : (
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
               {navLinks.map((link) => (
                 <li key={link.href}>
                   <Link
