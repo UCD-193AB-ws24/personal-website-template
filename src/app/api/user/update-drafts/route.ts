702f9b86183be2889838f4eaa95d525aca95bfcc
Christopher <chris_p9974@yahoo.com>
2025-03-02 17:34:49 -0800

    refactor(drafts): create drafts using new pages schema

--- src/app/api/user/update-drafts/route.ts
+++ src/app/api/user/update-drafts/route.ts
@@ -3,51 +3,49 @@ import { getCurrentUser, db } from '@lib/firebase/firebaseAdmin';
 import { APIResponse } from '@customTypes/apiResponse';
 import { FieldValue } from 'firebase-admin/firestore';
 
-
 // POST /api/user/update-drafts:
 // Update drafts with new id
 export async function POST(req: NextRequest) {
-  try {
-    const reqJson = await req.json();
+	try {
+		const reqJson = await req.json();
 
-    if (!reqJson.hasOwnProperty("timestamp")) {
-      throw new Error("Invalid request");
-    }
+		if (!reqJson.hasOwnProperty('timestamp')) {
+			throw new Error('Invalid request');
+		}
 
-    // Get the timestamp
-    const timestamp = reqJson["timestamp"];
+		// Get the timestamp
+		const timestamp = reqJson['timestamp'];
 
-    // Get the user who sent the request and their uid to get their user document
-    const user = await getCurrentUser();
-    if (user === null) {
-      throw new Error("No user found");
-    }
-    const userDoc = db.collection('users').doc(user.uid);
+		// Get the user who sent the request and their uid to get their user document
+		const user = await getCurrentUser();
+		if (user === null) {
+			throw new Error('No user found');
+		}
+		const userDoc = db.collection('users').doc(user.uid);
 
-    // Append the timestamp to the user's to draftIds
-    await userDoc.update({
-      draftMappings: FieldValue.arrayUnion(
-        {
-          id: timestamp,
-          name: "Untitled Draft"
-        }
-      )
-    });
+		// Append the timestamp to the user's to draftIds
+		await userDoc.update({
+			draftMappings: FieldValue.arrayUnion({
+				id: timestamp,
+				name: 'Untitled Draft',
+			}),
+		});
 
-    // Create the new draft
-    const draftsRef = db.collection('drafts');
-    await draftsRef.add({
-      draftId:`${user.uid}-${timestamp}`,
-      components: []
-    })
+		// Create the new draft
+		const draftsRef = db.collection('drafts');
+		await draftsRef.add({
+			draftId: `${user.uid}-${timestamp}`,
+			pages: [{ components: [], pageName: 'Home' }],
+		});
 
-    return NextResponse.json<APIResponse<string>>({
-      success: true, data: "Success"
-    });
-  } catch (error: any) {
-    return NextResponse.json<APIResponse<string>>(
-      { success: false, error: error.message },
-      { status: 400 }
-    )
-  }
+		return NextResponse.json<APIResponse<string>>({
+			success: true,
+			data: 'Success',
+		});
+	} catch (error: any) {
+		return NextResponse.json<APIResponse<string>>(
+			{ success: false, error: error.message },
+			{ status: 400 }
+		);
+	}
 }
