29d792e7857360ae6f346795e5ebe148fe6aff7f
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2025-03-03 21:35:40 -0800

    feat(image): save images to cloud storage

--- src/lib/firebase/firebaseApp.ts
+++ src/lib/firebase/firebaseApp.ts
@@ -1,13 +1,15 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import firebaseConfig from "./firebaseConfig";
 
 // Ensure Firebase isn't initialized multiple times
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Initialize Firebase Authentication and Firestore
+// Initialize Firebase Authentication, Firestore, and Firebase Storage
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app)
 
-export { auth, db };
+export { auth, db, storage};
