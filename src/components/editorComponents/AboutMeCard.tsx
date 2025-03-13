031b8aae1f667e8f71b6a4c35b6ed3680126cf9f
Hanson <Hansonlau7@gmail.com>
2025-03-13 01:31:15 -0700

    initial work on about me card

---
+++ src/components/editorComponents/AboutMeCard.tsx
@@ -0,0 +1,349 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Rnd } from "react-rnd";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useSearchParams } from "next/navigation";
+
+import ActiveOutlineContainer from "@components/editorComponents/ActiveOutlineContainer";
+import SkeletonLoader from "@components/editorComponents/SkeletonLoader";
+
+import type {
+  ComponentItem,
+  Position,
+  Size,
+} from "@customTypes/componentTypes";
+
+import { handleDragStop, handleResizeStop } from "@utils/dragResizeUtils";
+import { GRID_SIZE } from "@utils/constants";
+import { auth, storage } from "@lib/firebase/firebaseApp";
+
+interface AboutMeCardContent {
+  image: string;
+  bio: string;
+  contact: string;
+}
+
+interface AboutMeCardProps {
+  id?: string;
+  initialPos?: Position;
+  initialSize?: Size;
+  components?: ComponentItem[];
+  content?: any;
+  updateComponent?: (
+    id: string,
+    newPos: Position,
+    newSize: Size,
+    content?: any
+  ) => void;
+  isActive?: boolean;
+  onMouseDown?: () => void;
+  setIsDragging?: (dragging: boolean) => void;
+  isPreview?: boolean;
+}
+
+export default function AboutMeCard({
+  id = "",
+  initialPos = { x: -1, y: -1 },
+  initialSize = { width: 300, height: 150 },
+  components = [],
+  content = "",
+  updateComponent = () => {},
+  isActive = true,
+  onMouseDown: onMouseDown = () => {},
+  setIsDragging = () => {},
+  isPreview = false,
+}: AboutMeCardProps) {
+  const [position, setPosition] = useState(initialPos);
+  const [size, setSize] = useState(initialSize);
+  const [curContent, setCurContent] = useState({
+    image: "No Image",
+    bio: "Enter Bio here",
+    contact: "email@email.com | (123)-456-7890",
+  });
+
+  const [imageSrc, setImageSrc] = useState("");
+  const [loading, setLoading] = useState(!!content);
+  const [previewSrc, setPreviewSrc] = useState<string | null>(null);
+
+  const draftNumber = useSearchParams().get("draftNumber");
+
+
+  useEffect(() => {
+    try {
+      const jsonContent = JSON.parse(content);
+      setCurContent(jsonContent);
+
+    } catch (e) {
+      setCurContent({
+        image: "No Image",
+        bio: "Enter Bio here",
+        contact: "email@email.com | (123)-456-7890",
+      });
+    }
+  }, []);
+
+  const handleMouseDown = (e: MouseEvent) => {
+    e.stopPropagation();
+    onMouseDown();
+  };
+
+  const resizeImage = (
+    imageSrc: string,
+    baseWidth: number = 300
+  ): Promise<{ width: number; height: number }> => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = imageSrc;
+      img.onload = () => {
+        const aspectRatio = img.width / img.height;
+        const newWidth = baseWidth;
+        const newHeight = newWidth / aspectRatio;
+        resolve({ width: newWidth, height: newHeight });
+      };
+    });
+  };
+
+  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userId = auth.currentUser?.uid;
+
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const filePath = `users/${userId}/drafts/${draftNumber}/${id}-${file.name}`;
+      const storageRef = ref(storage, filePath);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      const localPreview = URL.createObjectURL(file);
+      setPreviewSrc(localPreview);
+      setLoading(false);
+
+      // Cleanup previous preview when component re-renders
+      if (previewSrc) {
+        URL.revokeObjectURL(previewSrc);
+      }
+
+      let sizeSet = false;
+      const size = await resizeImage(localPreview);
+      setSize(size);
+      sizeSet = true;
+
+      uploadTask.on(
+        "state_changed",
+        null,
+        (error) => {
+          console.error("Upload failed:", error);
+          setLoading(false);
+          setPreviewSrc(null);
+        },
+        async () => {
+          // Get the download URL once uploaded
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          setImageSrc(downloadURL);
+          setPreviewSrc(null);
+
+          if (!sizeSet) {
+            const { width, height } = await resizeImage(downloadURL);
+            setSize({ width, height });
+          }
+
+          setCurContent((prevContent: AboutMeCardContent) => ({
+            ...prevContent,
+            image: downloadURL,
+          }));
+          updateComponent(
+            id,
+            position,
+            size,
+            JSON.stringify({
+              ...curContent,
+              image: downloadURL,
+            })
+          );
+        }
+      );
+    }
+  };
+
+  return isPreview ? (
+    <div
+      style={{
+        position: "absolute",
+        left: position.x,
+        top: position.y,
+        width: size.width,
+        height: size.height,
+        padding: `${GRID_SIZE}px`,
+      }}
+      className="flex w-full h-full justify-between whitespace-pre-wrap bg-transparent overflow-hidden resize-none text-lg"
+    >
+      {loading && <SkeletonLoader width={size.width} height={size.height} />}
+      {previewSrc ? (
+        <img
+          src={previewSrc}
+          alt="Uploading Preview"
+          className="w-full h-full object-cover"
+        />
+      ) : curContent.image != "No Image" ? (
+        <img
+          src={curContent.image}
+          alt="Uploaded"
+          className="w-full h-full object-cover"
+          onLoad={() => setLoading(false)}
+          onError={() => setLoading(false)}
+          style={{ display: loading ? "none" : "block" }}
+        />
+      ) : (
+        <div className="w-full h-full flex items-center justify-center bg-gray-200">
+          No Image
+        </div>
+      )}
+
+      <div className="flex flex-col justify-between">
+        <p
+          draggable="false"
+          className="overflow-hidden min-w-[300px] max-w-[300px] max-h-[30px] text-black text-2xl font-bold cursor-text p-0 m-0 leading-none"
+        >
+          {curContent.bio}
+        </p>
+
+        <p
+          draggable="false"
+          className="overflow-hidden min-h-[24px] max-h-[24px] min-w-[300px] max-w-[300px]"
+        >
+          {curContent.contact}
+        </p>
+      </div>
+    </div>
+  ) : (
+    <Rnd
+      size={{ width: size.width, height: size.height }}
+      position={{ x: position.x, y: position.y }}
+      onDragStart={() => {
+        setIsDragging(true);
+      }}
+      onDragStop={(e, d) => {
+        setIsDragging(false);
+        handleDragStop(
+          id,
+          size,
+          components,
+          updateComponent,
+          setPosition
+        )(e, d);
+      }}
+      onResizeStart={() => setIsDragging(true)}
+      onResizeStop={(e, d, ref, delta, newPosition) => {
+        setIsDragging(false);
+        handleResizeStop(id, components, updateComponent, setSize, setPosition)(
+          e,
+          d,
+          ref,
+          delta,
+          newPosition
+        );
+      }}
+      minHeight={100}
+      minWidth={600}
+      bounds="parent"
+      onMouseDown={handleMouseDown}
+      style={{ pointerEvents: "auto" }}
+      dragGrid={[GRID_SIZE, GRID_SIZE]}
+      resizeGrid={[GRID_SIZE, GRID_SIZE]}
+    >
+      <ActiveOutlineContainer isActive={isActive}>
+        <div
+          className="flex w-full h-full justify-between whitespace-pre-wrap bg-transparent overflow-hidden resize-none text-lg"
+          style={{ padding: `${GRID_SIZE}px` }}
+        >
+          {loading && (
+            <SkeletonLoader width={size.width} height={size.height} />
+          )}
+
+          {previewSrc ? (
+            <img
+              src={previewSrc}
+              alt="Uploading Preview"
+              className="min-h-[200px] max-h-[300px] min-w-[300px] max-w-[300px]"
+            />
+          ) : curContent.image != "No Image" ? (
+            <img
+              src={curContent.image}
+              alt="Uploaded"
+              className="min-h-[200px] max-h-[300px] min-w-[300px] max-w-[300px]"
+              onLoad={() => setLoading(false)}
+              onError={() => setLoading(false)}
+              style={{ display: loading ? "none" : "block" }}
+            />
+          ) 
+          : (
+            <label className="min-h-[200px] max-h-[300px] min-w-[300px] max-w-[300px] flex items-center justify-center cursor-pointer bg-gray-200">
+              Click to Upload an Image
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                className="hidden"
+              />
+            </label>
+          )}
+
+          <div className="flex flex-col justify-between">
+            <p
+              contentEditable
+              suppressContentEditableWarning
+              draggable="false"
+              className="overflow-hidden min-h-[24px] max-h-[24px] min-w-[300px] max-w-[300px] outline outline-gray-300 rounded-sm"
+              style={{
+                outline: `${!isActive ? "none" : ""}`,
+              }}
+              onBlur={(e) => {
+                setCurContent((prevContent: AboutMeCardContent) => ({
+                  ...prevContent,
+                  bio: e.target.innerText,
+                }));
+                updateComponent(
+                  id,
+                  position,
+                  size,
+                  JSON.stringify({
+                    ...curContent,
+                    bio: e.target.innerText,
+                  })
+                );
+              }}
+            >
+              {curContent.bio}
+            </p>
+
+            <p
+              contentEditable
+              suppressContentEditableWarning
+              draggable="false"
+              className="overflow-hidden min-w-[300px] max-w-[300px] resize-none bg-transparent text-lg leading-none outline outline-gray-300 rounded-sm"
+              style={{
+                outline: `${!isActive ? "none" : ""}`,
+              }}
+              onBlur={(e) => {
+                setCurContent((prevContent: AboutMeCardContent) => ({
+                  ...prevContent,
+                  contact: e.target.innerText,
+                }));
+                updateComponent(
+                  id,
+                  position,
+                  size,
+                  JSON.stringify({
+                    ...curContent,
+                    contact: e.target.innerText,
+                  })
+                );
+              }}
+            >
+              {curContent.contact}
+            </p>
+          </div>
+        </div>
+      </ActiveOutlineContainer>
+    </Rnd>
+  );
+}
--- src/utils/componentUtils.tsx
+++ src/utils/componentUtils.tsx
@@ -10,6 +10,7 @@ import WorkEntry from '@components/editorComponents/WorkEntry';
 import HorizontalLine from '@components/editorComponents/HorizontalLine';
 import VerticalLine from '@components/editorComponents/VerticalLine';
 import IconComponent from '@components/editorComponents/IconComponent';
+import AboutMeCard from "@components/editorComponents/AboutMeCard";
 
 import { ComponentItem } from '@customTypes/componentTypes';
 
@@ -29,22 +30,23 @@ export const componentMap: Record<
   horizontalLine: HorizontalLine,
   verticalLine: VerticalLine,
   icon: IconComponent,
+  aboutMeCard: AboutMeCard,
 };
 
 export const componentSizes: Record<string, { width: number; height: number }> =
-	{
-		textBlock: { width: 200, height: 150 },
-		sectionTitle: { width: 350, height: 30 },
-		image: { width: 200, height: 150 },
-		file: { width: 425, height: 550 },
-		video: { width: 450, height: 250 },
-		navBar: { width: 100000, height: 48 },
-		academicEntry: { width: 620, height: 70 },
-		webPage: { width: 800, height: 600 },
-		workEntry: { width: 750, height: 150 },
-		horizontalLine: { width: 350, height: 2 },
-		verticalLine: { width: 2, height: 350 },
-    	icon: { width: 100, height: 100}
+  {
+    textBlock: { width: 200, height: 150 },
+    sectionTitle: { width: 350, height: 30 },
+    image: { width: 200, height: 150 },
+    file: { width: 425, height: 550 },
+    video: { width: 450, height: 250 },
+    navBar: { width: 100000, height: 48 },
+    academicEntry: { width: 600, height: 70 },
+    webPage: { width: 800, height: 600 },
+    horizontalLine: { width: 800, height: 4 },
+    verticalLine: { width: 4, height: 800 },
+    icon: { width: 100, height: 100},
+    aboutMeCard: { width: 600, height: 70 }
   };
 
 export const renderOverlayContent = (activeType: string | null) => {
@@ -73,6 +75,8 @@ export const renderOverlayContent = (activeType: string | null) => {
       return <VerticalLine />;
     case "icon":
       return <IconComponent />;
+    case "aboutMeCard":
+      return <AboutMeCard />
     default:
       return null;
   }
