0fd52124c25d1bb5775670fea82c2786a0ab0b42
Hanson <Hansonlau7@gmail.com>
2025-04-30 00:36:17 -0700

    test(authPages): added tests for login and signup page

---
+++ __tests__/authPages.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import LogInForm from '../src/app/login/loginForm'
+import SignUpForm from '../src/app/signup/signupForm'
+
+import { expect, test } from 'vitest';
+
+test('login buttons', () => {
+    render(<LogInForm />)
+
+    const buttons = screen.getAllByTestId('log-in-btn');
+    expect(buttons).toHaveLength(2);
+});
+
+test('signup buttons', () => {
+    render(<SignUpForm />)
+
+    const buttons = screen.getAllByTestId('sign-up-btn');
+    expect(buttons).toHaveLength(2);
+});
--- src/app/signup/signupForm.tsx
+++ src/app/signup/signupForm.tsx
@@ -88,6 +88,7 @@ export default function SignUpForm() {
             required
           />
           <button
+            data-testid="sign-up-btn"
             type="submit"
             className="relative px-6 py-4 font-semibold text-white bg-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#d67500] hover:border-[#d67500] shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50"
           >
@@ -105,6 +106,7 @@ export default function SignUpForm() {
         {/* Google Sign-In Button */}
         <div className="flex flex-col items-center gap-2 mt-4">
           <button
+            data-testid="sign-up-btn"
             onClick={handleSignInWithGoogle}
             className="flex items-center w-full max-w-xs px-4 py-3 bg-white border border-gray-300 rounded-md shadow-md transition duration-300 hover:bg-gray-100 focus:outline-none"
           >
