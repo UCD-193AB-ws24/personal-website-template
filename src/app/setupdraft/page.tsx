337a7f5014ded4a2f8009a565c45e796c5fe4ab7
Christopher <chris_p9974@yahoo.com>
2025-05-12 21:03:43 -0700

    feat(setupdraft): change to column layout for mobile view

--- src/app/setupdraft/page.tsx
+++ src/app/setupdraft/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useAuthState } from "react-firebase-hooks/auth";
-// import { auth } from "@lib/firebase/firebaseApp";
 import { getFirebaseAuth } from "@lib/firebase/firebaseApp";
 const auth = getFirebaseAuth();
 
@@ -167,67 +166,65 @@ export default function SetupDraft() {
         )}
       </header>
 
-      <main className="mx-auto max-w-screen-xl p-4">
+      <main className="mx-auto p-4">
         <LoadingSpinner show={isLoading} />
-        <div className="flex w-full max-w-screen-xl p-4 h-1/2">
-          <div className="flex w-full max-w-screen-xl p-4">
-            {/* Left Content */}
-            <div className="flex-1 flex justify-center">
-              <div className="w-full max-w-md p-16 text-white bg-[#111827] shadow-md flex flex-col justify-between rounded-md">
-                <div>
-                  <h1 className="text-xl font-semibold">Choose a template</h1>
+        <div className="flex flex-col md:flex-row gap-y-4 lg:gap-x-16 w-full p-8">
+          {/* Left Content */}
+          <div className="flex-1 flex justify-center md:justify-end">
+            <div className="w-full max-w-md p-16 text-white bg-[#111827] shadow-md flex flex-col justify-between rounded-md">
+              <div>
+                <h1 className="text-xl font-semibold">Choose a template</h1>
 
-                  <p className="mt-4">
-                    Want some inspiration for your website? Find a template to
-                    quickstart your draft!
-                  </p>
-                </div>
+                <p className="mt-4">
+                  Want some inspiration for your website? Find a template to
+                  quickstart your draft!
+                </p>
+              </div>
 
-                <div className="flex justify-center mt-16">
-                  <button
-                    data-testid="new-draft-btn"
-                    onClick={() => {
-                      if (!user) {
-                        router.push("/login");
-                      } else {
-                        router.push("/templates");
-                      }
-                    }}
-                    className="relative inline-flex px-6 py-4 w-1/2 text-md font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 items-center justify-center text-center"
-                  >
-                    Templates
-                  </button>
-                </div>
+              <div className="flex justify-center mt-16">
+                <button
+                  data-testid="new-draft-btn"
+                  onClick={() => {
+                    if (!user) {
+                      router.push("/login");
+                    } else {
+                      router.push("/templates");
+                    }
+                  }}
+                  className="relative inline-flex px-6 py-4 w-1/2 text-md font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 items-center justify-center text-center"
+                >
+                  Templates
+                </button>
               </div>
             </div>
+          </div>
 
-            {/* Vertical Separator */}
-            <div className="w-1 bg-gray-400 mx-4"></div>
+          {/* Vertical Separator */}
+          <div className="w-1 invisible md:visible bg-gray-400 mx-4"></div>
 
-            {/* Right Content */}
-            <div className="flex-1 flex justify-center">
-              <div className="w-full max-w-md p-16 text-white bg-[#111827] shadow-md flex flex-col justify-between rounded-md">
-                <div>
-                  <h1 className="text-xl font-semibold">Start from scratch</h1>
+          {/* Right Content */}
+          <div className="flex-1 flex justify-center md:justify-start">
+            <div className="w-full max-w-md p-16 text-white bg-[#111827] shadow-md flex flex-col justify-between rounded-md">
+              <div>
+                <h1 className="text-xl font-semibold">Start from scratch</h1>
 
-                  <p className="mt-4">
-                    What better joy than making your own idea come to life?
-                    Start with a clean canvas!
-                  </p>
-                </div>
+                <p className="mt-4">
+                  What better joy than making your own idea come to life? Start
+                  with a clean canvas!
+                </p>
+              </div>
 
-                <div className="flex justify-center mt-16">
-                  <button
-                    data-testid="select-template-btn"
-                    onClick={() => {
-                      setIsModalHidden(false);
-                      handleNewDraft();
-                    }}
-                    className="relative inline-flex px-6 py-4 w-1/2 text-md font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 items-center justify-center text-center"
-                  >
-                    Create
-                  </button>
-                </div>
+              <div className="flex justify-center mt-16">
+                <button
+                  data-testid="select-template-btn"
+                  onClick={() => {
+                    setIsModalHidden(false);
+                    handleNewDraft();
+                  }}
+                  className="relative inline-flex px-6 py-4 w-1/2 text-md font-semibold text-[#f08700] border border-[#f08700] rounded-md transition-all duration-300 hover:bg-[#f08700] hover:text-black shadow-[0_0_10px_rgba(240,135,0,0.4)] hover:shadow-[0_0_15px_rgba(240,135,0,0.6)] before:absolute before:inset-0 before:border-2 before:border-[#f08700] before:rounded-md before:opacity-10 before:scale-95 hover:before:scale-100 hover:before:opacity-50 items-center justify-center text-center"
+                >
+                  Create
+                </button>
               </div>
             </div>
           </div>
